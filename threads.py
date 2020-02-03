5c865cd7db2b20cce82ceda6b3e0a2f60ec41bed
Anton Smerdov <neonx11ant@gmail.com>
2020-02-03 14:31:48 +0300

    fixed time formatting for the time thread

--- threads.py
+++ threads.py
@@ -358,6 +358,11 @@ class MeasurementsThread(SocketThread):
                 # measurement_data = [str(value) for value in measurement_data]  # Commented on 31 Oct to fix rounding
                 for i in range(1, len(measurement_data)):  # Rounding to exactly 3 digits
                     measurement_data[i] = "%.3f" % measurement_data[i]
+                    ### It looks like the trick above is not enough to round to exactly 3 digits.
+                    ### That's why I added the code below
+                    value_parts = measurement_data[i].split('.')
+                    value_parts[1] = value_parts[1][:3]  # Cutting everything after the 3rd digit
+                    measurement_data[i] = '.'.join(value_parts)
                     # measurement_data[i] = "%.3f" % measurement_data[i]  # TODO: check in google why rounding isn't working
 
                 data2write = ','.join(measurement_data) + '\n'
